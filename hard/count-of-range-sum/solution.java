6345b2527c5c43d1d4d92ce17806b4fa2f11e4d3
Secret Glimmer <erik0304andersson@gmail.com>
2024-03-29 17:56:08 +0100

    feat: add a solution

---
+++ hard/count-of-range-sum/solution.java
@@ -0,0 +1,12 @@
+public int countRangeSum(int[] nums, int lower, int upper) {
+    int n = nums.length;
+    long[] sums = new long[n + 1];
+    for (int i = 0; i < n; ++i)
+        sums[i + 1] = sums[i] + nums[i];
+    int ans = 0;
+    for (int i = 0; i < n; ++i)
+        for (int j = i + 1; j <= n; ++j)
+            if (sums[j] - sums[i] >= lower && sums[j] - sums[i] <= upper)
+                ans++;
+    return ans;
+}
