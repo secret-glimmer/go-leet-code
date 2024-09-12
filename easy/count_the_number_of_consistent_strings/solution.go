65230009ae89b6bf3f792e2196a90715eeae6984
Secret Glimmer <erik0304andersson@gmail.com>
2024-09-12 11:00:48 +0200

    feat: update count_the_number_of_consistent_strings solution

--- easy/count_the_number_of_consistent_strings/solution.go
+++ easy/count_the_number_of_consistent_strings/solution.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func countConsistentStrings(allowed string, words []string) int {
+	allowedSet := make(map[rune]struct{})
+	for _, char := range allowed {
+		allowedSet[char] = struct{}{}
+	}
+
+	count := 0
+	for _, word := range words {
+		isConsistent := true
+		for _, char := range word {
+			if _, exists := allowedSet[char]; !exists {
+				isConsistent = false
+				break
+			}
+		}
+		if isConsistent {
+			count++
+		}
+	}
+
+	return count
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	allowed1 := "ab"
+	words1 := []string{"ad", "bd", "aaab", "baa", "badab"}
+	fmt.Println(countConsistentStrings(allowed1, words1)) // Output: 2
+
+	allowed2 := "abc"
+	words2 := []string{"a", "b", "c", "ab", "ac", "bc", "abc"}
+	fmt.Println(countConsistentStrings(allowed2, words2)) // Output: 7
+
+	allowed3 := "cad"
+	words3 := []string{"cc", "acd", "b", "ba", "bac", "bad", "ac", "d"}
+	fmt.Println(countConsistentStrings(allowed3, words3)) // Output: 4
 }
